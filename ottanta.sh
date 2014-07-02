c0125e911ff6a50df27820d571e4b4ff2f349fc2
Guy Pursey <guypursey@gmail.com>
2014-07-02 07:22:16 +0100

    Add function and loop to break lines from file.
    After some experimenting with recursion, I created
    a function that can take a string and return a string
    with line-breaks so that no line is longer than eighty
    characters.
    
    A simple loop under the function takes the filename,
    finds the file, and uses the function to produce an
    echo of the newly formatted file contents.

--- ottanta.sh
+++ ottanta.sh
@@ -3,3 +3,39 @@
 # Script which takes a markdown file and reproduces it
 # with each line reduced to eighty characters or
 # fewer.
+
+function breakline {
+	local text=$1
+	local rtn=""
+	while [[ ${#text} -gt 0 ]]; do
+		if [[ ${#text} -gt 80 ]]; then
+			local COUNTER=80
+			local spaceloc=0
+			local delimiter="\n"
+			while [[ $spaceloc -eq 0 ]]; do
+				if [[ "${text:$COUNTER:1}" = " " ]]; then
+					let spaceloc=$COUNTER
+				fi
+				let COUNTER-=1
+				if [[ $COUNTER -eq 0 ]]; then
+					let spaceloc=79
+					delimiter="-"$delimiter
+				fi
+			done
+
+			rtn=$rtn${text:0:$spaceloc}$"$delimiter"
+			text=${text:$spaceloc}
+		else
+			rtn=$rtn$text
+			text=""
+		fi
+	done
+
+	#return
+	echo -e $rtn
+}
+
+while read line
+do
+	echo -e $"$(breakline "$line")"
+done < $1
